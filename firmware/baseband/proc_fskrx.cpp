be790c83393e310a874d7cdd875ffd0f1383f6a3
bencejuhaasz <32618702+bencejuhaasz@users.noreply.github.com>
2022-10-27 14:43:08 +0200

    Created skeleton code for FSK_RX baseband process

---
+++ firmware/baseband/proc_fskrx.cpp
@@ -0,0 +1,15 @@
+#include "proc_fskrx.hpp"
+#include "portapack_shared_memory.hpp"
+#include "sine_table_int8.hpp"
+#include "event_m4.hpp"
+
+#include <cstdint>
+
+
+void FSKRXProcessor::execute(const buffer_c8_t& buffer) {
+
+}
+
+void FSKProcessor::on_message(const Message* const p) {
+
+}
---
+++ firmware/baseband/proc_fskrx.hpp
@@ -0,0 +1,13 @@
+#ifndef __PROC_FSKRX_H__
+#define __PROC_FSKRX_H__
+
+#include "baseband_processor.hpp"
+#include "baseband_thread.hpp"
+
+class FSKRXProcessor: public BasebandProcessor {
+public:
+  void execute(const buffer_c8_t& buffer) override;
+  void on_message(const Message* const p) override;
+}
+
+#endif
